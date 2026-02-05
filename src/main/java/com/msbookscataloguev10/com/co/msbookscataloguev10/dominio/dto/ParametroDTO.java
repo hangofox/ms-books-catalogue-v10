f370c8a9f120dcb18cc67d3f103be60525501f9d
Malena18 <malenap_90@hotmail.com>
2026-02-04 20:31:59 -0500

    Se realizo las operaciones de la tabla parametros

---
+++ src/main/java/com/msbookscataloguev10/com/co/msbookscataloguev10/dominio/dto/ParametroDTO.java
@@ -0,0 +1,7 @@
+package com.msbookscataloguev10.com.co.msbookscataloguev10.dominio.dto;
+import lombok.Data;
+@Data
+public class ParametroDTO {
+    private Long idParametro;
+    private String urlBaseLibro;
+}
